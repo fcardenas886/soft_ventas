bd9e042c63253dfbc79f19123bebf96d3df6c484
fcardenas886 <alek_886@hotmail.com>
2023-10-29 02:25:00 -0300

    creacion de proyecto

---
+++ Git_ventas/src/main/java/com/mycompany/git_ventas/Git_ventas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.git_ventas;
+
+/**
+ *
+ * @author Alexander
+ */
+public class Git_ventas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
